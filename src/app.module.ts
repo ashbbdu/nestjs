5a25e419d6fdc17865361103ddb85f0356ddfe39
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-12 15:57:16 +0530

    code cleaning

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,33 +1,23 @@
-// import { Module } from '@nestjs/common';
-// import { AppController } from './app.controller';
-// import { AppService } from './app.service';
-// import { TypeOrmModule } from '@nestjs/typeorm';
-
-// @Module({
-//   imports: [
-//     TypeOrmModule.forRoot({
-//       type: 'mysql',
-//       host: 'localhost',
-//       port: 3306,
-//       username: 'root',
-//       password: 'ash@Compunnel09',
-//       database: 'dashboard-clone',
-//       entities: [],
-//       synchronize: true,
-//     }),
-//   ],
-//   controllers: [AppController],
-//   providers: [AppService],
-// })
-// export class AppModule {}
-
-
-
 import { Module } from '@nestjs/common';
-import { CatsModule } from './cats/cats.module';
-
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CatsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'ash@Compunnel09',
+      database: 'dashboard-clone',
+      entities: [],
+      synchronize: true,
+    }),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
+
