ec4b08ce84bf13f851644a963009816e057189a4
ashbbdu <ashishsrivastava7007@gmail.com>
2025-05-12 15:39:24 +0530

    initial push nest js

---
+++ src/app.module.ts
@@ -0,0 +1,33 @@
+// import { Module } from '@nestjs/common';
+// import { AppController } from './app.controller';
+// import { AppService } from './app.service';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+
+// @Module({
+//   imports: [
+//     TypeOrmModule.forRoot({
+//       type: 'mysql',
+//       host: 'localhost',
+//       port: 3306,
+//       username: 'root',
+//       password: 'ash@Compunnel09',
+//       database: 'dashboard-clone',
+//       entities: [],
+//       synchronize: true,
+//     }),
+//   ],
+//   controllers: [AppController],
+//   providers: [AppService],
+// })
+// export class AppModule {}
+
+
+
+import { Module } from '@nestjs/common';
+import { CatsModule } from './cats/cats.module';
+
+
+@Module({
+  imports: [CatsModule],
+})
+export class AppModule {}
